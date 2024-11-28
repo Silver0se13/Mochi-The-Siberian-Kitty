cba3fa7ec7064a1b219b975ed0295978530db612
Silver0se13 <gurleens0013@gmail.com>
2024-11-27 22:58:55 -0700

    added ids and started button functions for img
    Created mochi.js
    added ids to the h tags in html
    Started button functions for changing img in js

---
+++ mochi.js
@@ -0,0 +1,19 @@
+about.addEventListener('click', function() {
+    console.log("About was pressed.");
+    cat.src = "images/Mochi_Comfy.jpeg";
+})
+
+home.addEventListener('click', function() {
+    console.log("Coming Home was pressed.");
+    cat.src = "";
+})
+
+weeks.addEventListener('click', function() {
+    console.log("First Few Weeks was pressed.");
+    cat.src = "";
+})
+
+today.addEventListener('click', function() {
+    console.log("Growing Today was pressed.");
+    cat.src = "";
+})
