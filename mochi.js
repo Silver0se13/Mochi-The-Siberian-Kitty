31d12d063a582daf4f7a317da81df17c06de1b02
Silver0se13 <gurleens0013@gmail.com>
2024-11-29 00:03:30 -0700

    changed images; edited js text content; added media queries
    changed images; edited js text content; added media queries in css

--- mochi.js
+++ mochi.js
@@ -1,6 +1,6 @@
 about.addEventListener('click', function() {
     console.log("About was pressed.");
-    cat.src = "images/Mochi_Comfy.jpeg";
+    cat.src = "images/Pretty_Mochi.jpg";
     heading_two.innerHTML = "About Mochi";
     text.innerHTML = "Mochi is a cute and gorgeous little Siberian kitty. She has beautiful blue eyes and the fluffiest fur. Mochi's little nose is both pink and black, which her human mama thinks is unique. This kitty is very curious in nature, but does get scared of loud noises (like the vacuum and hair dryer). She's playful and loves cuddling up to her human mama. Mochi is very affectionate towards people she trusts. She's super sweet and hardly bites or scratches, only love bites. This little kitty is known for how much she purrs and how loud her purrs are. A pretty cool fact is that her birth parents are both pretty awesome, having won of titles in offical cat shows!"
     heading_three.innerHTML = "More Details About Mochi:"
@@ -9,27 +9,27 @@ about.addEventListener('click', function() {
 
 home.addEventListener('click', function() {
     console.log("Coming Home was pressed.");
-    cat.src = "images/Sleepy_Mochi.jpeg";
+    cat.src = "images/Mochi_Sleep_Pink.jpg";
     heading_two.innerHTML = "Coming Home";
-    text.innerHTML = ""
+    text.innerHTML = "Mochi was so small and super curious! She was also very shy and quiet, rarely meowing. The moment her carrier was opened, she took small cautious steps out and in into her new home. You could see her nose working, taking in all of the new scents. Mochi quickly got busy playing with all of her new toys. She ended up wearing herself out and took a relaxed and comfortable nap. This little kitty spent the whole day and night near her new human mama, and felt comfortable enough to cuddle up to her! <br> These first couple of days were exciting for the both of them."
     heading_three.innerHTML = ""
     list.innerHTML = ""
 })
 
 weeks.addEventListener('click', function() {
     console.log("First Few Weeks was pressed.");
-    cat.src = "";
-    heading_two.innerHTML = "First Few Weeks";
-    text.innerHTML = ""
+    cat.src = "images/Mochi_Outside.jpeg";
+    heading_two.innerHTML = "The First Few Weeks";
+    text.innerHTML = "Mochi soon became comfortable and use to her new home, and she was quick to learn her new name! Mochi slowly started harness training so she could enjoy outdoor walks from time to time. This kitty was quick on getting use to a harness and leash that before she new it, she was out exploring the front and back yard! Her fur pattern had started coming in more, her paws and face starting to darken. <br> During this time, Mochi also visited the vet for her vaccines, and she surprisingly handled it very well! In fact, she was purring while they were giving her the shot. She was just too busy eating a treat."
     heading_three.innerHTML = ""
     list.innerHTML = ""
 })
 
 today.addEventListener('click', function() {
     console.log("Growing Today was pressed.");
-    cat.src = "";
+    cat.src = "images/Mochi_Comfy.jpeg";
     heading_two.innerHTML = "Growing up Today";
-    text.innerHTML = ""
+    text.innerHTML = "Today, Mochi is growing up happy and healthy. She's still a little kitten, but she's grown so much in size compared to the first day home. Her fur colour is a lot more darker now as well, especially her face. Some parts of Mochi's white fur now has a cream-ish hue. Her tail is really fluffy now too. <br> Mochi is very clever. She's learned little tricks and games like 'peek-a-boo', 'fetch', and 'sit'. This little kitty understands words like 'good night' and 'bye-bye', as well as coming when being called (most of the time). <br> Mochi has also become a lot more vocal, meowing more often. She's still quiet majority of the time, but she no longer hesitates when wanting attention. <br> This little kitty is growing beautifully."
     heading_three.innerHTML = ""
     list.innerHTML = ""
 })
